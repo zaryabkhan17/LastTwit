b47b8849c872450b83a1f471843d1ca0a30713c5
zaryabkhan17 <74405564+zaryabkhan17@users.noreply.github.com>
2021-01-30 20:51:49 +0500

    lkjhgfdsdfghjk

--- server.js
+++ server.js
@@ -33,24 +33,13 @@ var upload = multer({ storage: storage })
 
 const admin = require("firebase-admin");
 // https://firebase.google.com/docs/storage/admin/start
-var SERVICE_ACCOUNT = JSON.parse({
-    "type": "service_account",
-    "project_id": "twitterapp-20ea6",
-    "private_key_id": "8e5e9a823728dac7cfc77f8698c54ed7a708cd55",
-    "private_key": "-----BEGIN PRIVATE KEY-----\nMIIEvQIBADANBgkqhkiG9w0BAQEFAASCBKcwggSjAgEAAoIBAQDq1YmV+jQx1pCh\n4p6irBgc6Be+dQlTLKhuaX1dlSUkts9E4FxuDykFb28x5ONxl1NXm48AkHxWa58Y\nFzXXmF+bAVe/aL6i2EYE+CN92MMZyMIaLtB4WJL8QYeykBmjFySvQ3U7MK+Rz0Fx\n8cTas7Su4B2H8dSbyAj7Q/XCKMDzN+KFOpsUlTUmHm9ePJdF0a7sgx6q928h/eX0\nbKlKD992hBOLYzlTAHOKokgMrkSQuUoRzX3BX5np8/Sg0pn/vb+IkNlCvIDEy+h3\ntUyu0owUJgp15Zn0v0TIb1qoGR5XQSWPH094lsvd1wqCHsv7VdoRGvjzNs8JhLFQ\nATbfO+O/AgMBAAECggEAA4335y1CXP7CcbDcddnlA4r/0QI0ge3LHuC55RC83zFl\nMcWzoHbaSBrHtBmi17VSraDNKVUGjMhUQCkZx4mm6NG8YklTmWNsq+aVovIFAf7n\n8T2EKk4s62SSiUFlIfLXu09qFttFzQ82DmN6d1UBBySdTdF7RmBgwmzk62tygDvw\n5HUtLFd00hkDyTBSWmmUVCU0CvbN0fBF88huWUbUpJ9TsELpibbfEQCS5eVIip6h\nafiY0MfrfI3Gqro0bC3BOE06ZhUSB89P3Aq4IiCFoaLlVPqvIu1T0ntH3ylQ+1GW\nRZ2+x6tHEErSZOCEpcXVkQNrbRjy7Yf2llLwsSZjAQKBgQD94zY5wdUhwUUZwCAl\nvKMRroAbVZHv6O9BpIJ2qKix+MdJPiSjao1Q/RUUih90pPcfDIWL7mF7NDsvlaqp\nZzGgRHt36jY0uzI+FCnzl3dT/KtNPH2ENoPSr69Ffk729kR1QZrDLJ4RNkKA3ago\nPL6HZerH4nMEiXH9zOzmth59tQKBgQDsyb3EBzulktOv4z6gUGzf5NecEIhkMUqK\nfcBbeue27DJjLqNIU7Lu+P/OvmHL0SIozQUpNeZPGo6l8igowUaZku+Fpxz6Qw9t\npwIDMgai47sBYrFYbJBp403tTqT0omOBBkmbamiKP94qwJ141KkKtUGganl2P1Oz\n9DvK+d5kIwKBgCNifXZDS58uvqC+JDzeM6QNPBGTNYnIX0A4224KR6i2UaAlpxmX\niiaVf1Cjh4rMSxzqs9oy2SdAulcBdw2xMjtyD/tAi1mCFZiNWuN2Ys0z0/Qp0ByT\nNIAyuCzpU7wbyqLFB/rXxxgDBfXL7tnczXk1fSlqqDkjpt2GqCXnkAxZAoGAREht\nG+GGFto/h2I9xhdLPkmMz6mI23PHbiaTWQ4ZUgvNAAML/Sh92iIeCq2mIBOACUoO\nKfMsJjWfW0V4xT/vLwxLXDxzw8ug4Fsf3nIHreOEIwMl5zzvbEmrSDq8oPtPPH7m\nrRXDbdK7K5L+FkElOjxI34y02B+s8KEGuSLTOgcCgYEAmYA9e590Alc4GV2c3mvF\nVJvd8O1fxemdfUnvoW/OrNkxTux1p7cG439/yecqiWfF4VfgxJP7IYVtetDXbtZ1\n4L2F18UXtuZ63OXuUDZTgcwAvZsu5fyVP6AbnXUfH2JE/qTfEJLghnK030Tt/I0n\nIVptSNwSzcxiEyqMzxGjhts=\n-----END PRIVATE KEY-----\n",
-    "client_email": "firebase-adminsdk-rfvs3@twitterapp-20ea6.iam.gserviceaccount.com",
-    "client_id": "111801977628283419040",
-    "auth_uri": "https://accounts.google.com/o/oauth2/auth",
-    "token_uri": "https://oauth2.googleapis.com/token",
-    "auth_provider_x509_cert_url": "https://www.googleapis.com/oauth2/v1/certs",
-    "client_x509_cert_url": "https://www.googleapis.com/robot/v1/metadata/x509/firebase-adminsdk-rfvs3%40twitterapp-20ea6.iam.gserviceaccount.com"
-})
+var SERVICE_ACCOUNT = JSON.parse(process.env.SERVICE_ACCOUNT)
 
 admin.initializeApp({
     credential: admin.credential.cert(SERVICE_ACCOUNT),
-    databaseURL: "https://twitterapp-20ea6-default-rtdb.firebaseio.com"
+    databaseURL: process.env.databaseURL
 });
-const bucket = admin.storage().bucket("gs://twitterapp-20ea6.appspot.com");
+const bucket = admin.storage().bucket(process.env.bucket);
 
 app.use(bodyParser.json());
 app.use(cookieParser());
